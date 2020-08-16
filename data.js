591143a15afeffd118c6593409e6f7749a26f7d8
unknown <bmalkes@gmail.com>
2020-08-16 18:21:16 +0300

    push1

---
+++ data.js
@@ -0,0 +1,75 @@
+module.exports=[
+    {
+        id:"netflix",
+        title:"Clone Netflix React",
+        featured:true,
+        site:"https://bmalkes.github.io/Clone-Netflix-React/"
+    },
+    {
+        id:"brafe",
+        title:"Site Brafe Html && Css",
+        featured:false,
+        site:"https://bmalkes.github.io/brafe-grid/"
+    },
+    {
+        id:"parallax",
+        title:"Parallax",
+        featured:true,
+        site:"https://bmalkes.github.io/Parallax/"
+    },
+    {
+        id:"project",
+        title:"Project Css",
+        featured:false,
+        site:"https://bmalkes.github.io/Project/"
+    },
+    {
+        id:"izhar",
+        title:"Personal trainner",
+        featured:true,
+        site:"https://bmalkes.github.io/izhar/"
+    },
+    {
+        id:"figma",
+        title:"School Project",
+        featured:false,
+        site:"https://bmalkes.github.io/figma/"
+    },
+    {
+        id:"wolfbest",
+        title:"WolfBeast project of Css Grid",
+        featured:false,
+        site:"https://bmalkes.github.io/WolfBeast/"
+    },
+    {
+        id:"gym-master",
+        title:"Gym-Master Project",
+        featured:false,
+        site:"https://gym-master1.herokuapp.com/instructors"
+    },
+    {
+        id:"ecollat",
+        title:"Ecollect",
+        featured:false,
+        site:"https://eccollect.herokuapp.com/"
+    },
+    {
+        id:"clock",
+        title:"Digital JS Clock",
+        featured:false,
+        site:"https://bmalkes.github.io/Clock/"
+    },
+    {
+        id:"felxflog",
+        title:"Flog with Flebox",
+        featured:false,
+        site:"https://bmalkes.github.io/FlexBlog/"
+    },
+    {
+        id:"foody",
+        title:"Foody recipes",
+        featured:false,
+        site:"https://foody-ad.herokuapp.com/"
+    },
+
+]
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+const videos = require("./data");
+
+server.use(express.static("public"));
+server.set("view engine", "njk");
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+
+server.get("/", function (req, res) {
+  const data = {
+    avatar_url: "",
+    name: "Bruno Malkes",
+    role:
+      "I'm a enthusiast of the best web & mobile development technologies.<br>Currently a student focused on OmniStack (Javascript, Node.js, ReactJS and React Native, Vue.Js).",
+    links: [
+      {
+        name: "Github",
+        url: "https://www.github.com/BmAlkes",
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/bruno-malkes-a05a72150/",
+      },
+      {
+        name: "Facebook",
+        url: "https://www.facebook.com/bmalkes/",
+      },
+      {
+        name: "WhatsApp",
+        url: "https://wa.me/9720549547355",
+      },
+    ],
+  };
+
+  return res.render("about", { data });
+});
+server.get("/portifolio", function (req, res) {
+  return res.render("portifolio", { items: videos });
+});
+
+server.get("/video", function (req, res) {
+  const id = req.query.id
+  const video = videos.find(function (video) {
+    return video.id === id
+  })
+  if (!video) {
+    return res.send("video not found");
+  }
+
+  return res.render("video", { item: video });
+});
+
+server.listen(5000, function () {
+  console.log("server is running");
+});
